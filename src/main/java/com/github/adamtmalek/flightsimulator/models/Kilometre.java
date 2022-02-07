25080fdd01c9e0936f867d305f14a7f22749a143
APMMonteiro <ap71@hw.ac.uk>
2022-02-07 18:25:24 +0000

    Kilometer record
    Test

--- src/main/java/com/github/adamtmalek/flightsimulator/models/Kilometre.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/Kilometre.java
@@ -1,5 +1,11 @@
 package com.github.adamtmalek.flightsimulator.models;
 
-public class Kilometre {
-
+record Kilometre(double kilometre) {
+	public Kilometre(double kilometre) {
+		if (kilometre <= 0) {
+			throw new java.lang.IllegalArgumentException(
+				String.format("Invalid distance must be greater than 0: %f", kilometre));
+		}
+		this.kilometre = kilometre;
+	}
 }
