6e340ff7d042e9ce1ba09aa7f1912381fd1e8a62
Ade20150 <91483078+Ade20150@users.noreply.github.com>
2022-02-07 19:18:02 +0000

    Testing

--- src/main/java/com/github/adamtmalek/flightsimulator/models/GramsPerKilometre.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/GramsPerKilometre.java
@@ -2,5 +2,13 @@ package com.github.adamtmalek.flightsimulator.models;
 
 
 public record GramsPerKilometre(double gramsPerKilometre) {
+	
 
+	public GramsPerKilometre(double gramsPerKilometre) {
+		if (gramsPerKilometre <= 0) {
+			throw new java.lang.IllegalArgumentException(
+				String.format("Invalid distance must be greater than 0: %f", gramsPerKilometre()));
+		}
+		this.gramsPerKilometre = gramsPerKilometre;
+	}
 }
