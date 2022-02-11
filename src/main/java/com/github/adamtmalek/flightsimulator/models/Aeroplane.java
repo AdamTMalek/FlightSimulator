8ee6c1e5f03b4e262c3346469779162868633af3
Adam Malek <malekadas@gmail.com>
2022-02-11 14:13:30 +0100

    Edit Aeroplane to make code compile
    We'll have to edit the data types to right classes after we finish implementing the secondary constructor.

--- src/main/java/com/github/adamtmalek/flightsimulator/models/Aeroplane.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/Aeroplane.java
@@ -1,6 +1,11 @@
 package com.github.adamtmalek.flightsimulator.models;
 
 
-public record Aeroplane(String model, String manufacturer,KilometrePerHour speed,LitresPerKilometre fuelConsumption) {
+import org.jetbrains.annotations.NotNull;
 
+public record Aeroplane(@NotNull String model,
+                        @NotNull String manufacturer,
+                        @NotNull String speed,
+                        @NotNull String fuelConsumption) {
+  // TODO: Change this to secondary constructor. Primary should have the final classes as data types.
 }
