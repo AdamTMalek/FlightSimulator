8bbb80d5136ce910a1e223d5b410a865c8332cbb
Adam Malek <malekadas@gmail.com>
2022-02-14 21:59:40 +0100

    Change FlightData to use List instead of generic Collection type

--- src/main/java/com/github/adamtmalek/flightsimulator/models/io/FlightData.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/io/FlightData.java
@@ -7,11 +7,11 @@ import com.github.adamtmalek.flightsimulator.models.Airport;
 import com.github.adamtmalek.flightsimulator.models.Flight;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
+import java.util.List;
 
-public record FlightData(@NotNull Collection<Airport> airports,
-                         @NotNull Collection<Airline> airlines,
-                         @NotNull Collection<Aeroplane> aeroplanes,
-                         @NotNull Collection<Flight> flights) {
+public record FlightData(@NotNull List<Airport> airports,
+                         @NotNull List<Airline> airlines,
+                         @NotNull List<Aeroplane> aeroplanes,
+                         @NotNull List<Flight> flights) {
 
 }
