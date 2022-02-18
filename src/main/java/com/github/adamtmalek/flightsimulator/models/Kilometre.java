00a245a31b2c2ee8bad721ca8eca51939542f788
Chris Hulme <chris_hulme99@outlook.com>
2022-02-18 10:24:42 +0000

    Removed default record constructors - it is valid to have <0 values in records.

--- src/main/java/com/github/adamtmalek/flightsimulator/models/Kilometre.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/Kilometre.java
@@ -1,14 +1,6 @@
 package com.github.adamtmalek.flightsimulator.models;
 
 public record Kilometre(double kilometre) {
-    public Kilometre(double kilometre) {
-        if (kilometre <= 0) {
-            throw new java.lang.IllegalArgumentException(
-                    String.format("Invalid distance must be greater than 0: %f", kilometre));
-        }
-        this.kilometre = kilometre;
-    }
-
 
     public Kilometre(String kilometre) {
         //double kilometre = Double.parseDouble("kilometre");
--- src/main/java/com/github/adamtmalek/flightsimulator/models/LitresPerKilometre.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/LitresPerKilometre.java
@@ -2,13 +2,6 @@ package com.github.adamtmalek.flightsimulator.models;
 
 
 public record LitresPerKilometre(double litresPerKilometre) {
-    public LitresPerKilometre(double litresPerKilometre) {
-        if (litresPerKilometre <= 0) {
-            throw new java.lang.IllegalArgumentException(
-                    String.format("Invalid amount must be greater than 0: %f", litresPerKilometre));
-        }
-        this.litresPerKilometre = litresPerKilometre;
-    }
 
     public LitresPerKilometre(String litresPerKilometre) {
         //double litresPerKilometre = Double.parseDouble("litresPerKilometre");
