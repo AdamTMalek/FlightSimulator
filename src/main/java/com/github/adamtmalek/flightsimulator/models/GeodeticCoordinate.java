719522affaa78fcda281ad67a241b47a559b4251
Chris Hulme <chris_hulme99@outlook.com>
2022-02-18 13:06:06 +0000

    Commented out code in geodetic coordinate record. Unsure of it's purpose

--- src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
@@ -1,17 +1,17 @@
 package com.github.adamtmalek.flightsimulator.models;
 
-public record GeodeticCoordinate(double latitude, double longitude) {
-	private boolean isDegree(double deg) {
-		// TODO
-		return true;
-	}
-	
-	public GeodeticCoordinate(double latitude, double longitude) {
-		if (!isDegree(latitude) || !isDegree(longitude)) {
-			throw new java.lang.IllegalArgumentException(
-					String.format("Invalid coordinates must be degrees: %f, %f", latitude, longitude));
-		}
-		this.latitude = latitude;
-		this.longitude = longitude;
-	}
+public record GeodeticCoordinate(Degrees latitude, Degrees longitude) {
+//    private boolean isDegree(double deg) {
+//        // TODO
+//        return true;
+//    }
+//
+//    public GeodeticCoordinate(double latitude, double longitude) {
+//        if (!isDegree(latitude) || !isDegree(longitude)) {
+//            throw new java.lang.IllegalArgumentException(
+//                    String.format("Invalid coordinates must be degrees: %f, %f", latitude, longitude));
+//        }
+//        this.latitude = latitude;
+//        this.longitude = longitude;
+//    }
 }
