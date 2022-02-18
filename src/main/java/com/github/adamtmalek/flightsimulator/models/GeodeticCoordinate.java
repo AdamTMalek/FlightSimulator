dd99c91dd2e89054a62c9c172256dee003c4b329
Chris Hulme <chris_hulme99@outlook.com>
2022-02-18 16:28:50 +0000

    Removed commented code

--- src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
@@ -19,22 +19,6 @@ public record GeodeticCoordinate(double latitude, double longitude) {
                 + Math.cos(Math.toRadians(coord.latitude))
                 * Math.cos(Math.toRadians(this.latitude))
                 * Math.cos(Math.toRadians(theta)))) * 60 * 1.1515 * 1.609344;
-//        final double EARTH_RADIUS = 6371.0;
-//
-//        var radianLatitudeA = Math.toRadians(coord.latitude());
-//        var radianLongitudeA = Math.toRadians(coord.longitude());
-//        var radianLatitudeB = Math.toRadians(this.latitude());
-//        var radianLongitudeB = Math.toRadians(this.longitude());
-//
-//        var deltaLatitude = Math.abs(radianLatitudeB - radianLatitudeA);
-//        var deltaLongitude = Math.abs(radianLongitudeB - radianLongitudeA);
-//
-//        var distance = (2.0 * EARTH_RADIUS
-//                * Math.asin(Math.sqrt(Math.pow(Math.sin(deltaLatitude / 2), 2)
-//                + Math.cos(coord.latitude())
-//                + Math.cos(this.latitude())
-//                * Math.pow(Math.sin(deltaLongitude / 2), 2))));
-//        return distance;
     }
 
 
