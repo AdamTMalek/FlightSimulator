27b87d6c273436a662fa32e21a085fd2fc05906c
Chris Hulme <chris_hulme99@outlook.com>
2022-02-18 15:48:03 +0000

    Moved calculate distance function from utils to geodetic coordinate class

--- src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/models/GeodeticCoordinate.java
@@ -5,4 +5,26 @@ package com.github.adamtmalek.flightsimulator.models;
  * @param longitude Units: Degrees
  */
 public record GeodeticCoordinate(double latitude, double longitude) {
+
+
+    public double calculateDistance(GeodeticCoordinate coord) {
+
+        final double EARTH_RADIUS = 6371.0;
+
+        var radianLatitudeA = Math.toRadians(coord.latitude());
+        var radianLongitudeA = Math.toRadians(coord.longitude());
+        var radianLatitudeB = Math.toRadians(this.latitude());
+        var radianLongitudeB = Math.toRadians(this.longitude());
+
+        var deltaLatitude = radianLatitudeB - radianLatitudeA;
+        var deltaLongitude = radianLongitudeB - radianLongitudeA;
+
+        return (2.0 * EARTH_RADIUS
+                * Math.asin(Math.sqrt(Math.sin(deltaLatitude / 2)
+                + Math.cos(coord.latitude())
+                * Math.cos(this.latitude())
+                * Math.sin(deltaLongitude / 2))));
+    }
+
+
 }
