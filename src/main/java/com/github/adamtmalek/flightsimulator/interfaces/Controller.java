3c3122688e4c8e5c87a513765d3e87e0bb7901ef
Chris Hulme <chris_hulme99@outlook.com>
2022-02-18 10:19:10 +0000

    Created Controller interface

---
+++ src/main/java/com/github/adamtmalek/flightsimulator/interfaces/Controller.java
@@ -0,0 +1,44 @@
+package com.github.adamtmalek.flightsimulator.interfaces;
+
+import com.github.adamtmalek.flightsimulator.models.Flight;
+import com.github.adamtmalek.flightsimulator.models.io.FileHandlerException;
+import com.github.adamtmalek.flightsimulator.models.io.FlightData;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public interface Controller {
+
+    // Read single directory for required files, using default naming scheme
+    // defined in FlightDataFileHandler.
+    public FlightData readFlightData(Path fileDirectoryPath) throws
+            IOException,
+            FileHandlerException;
+
+    // Read unique paths for required files.
+    public FlightData readFlightData(Path airportSourcePath,
+                                     Path aeroplaneSourcePath,
+                                     Path airlineSourcePath,
+                                     Path flightSourcePath) throws
+            IOException,
+            FileHandlerException;
+
+    public void writeFlightData(Path destinationPath);
+
+    public void writeAirlineReports(Path destinationPath);
+
+    public void addFlight(Flight flight) throws
+            UnsupportedOperationException,
+            ClassCastException,
+            NullPointerException,
+            IllegalArgumentException;
+
+    public void removeFlight(int index) throws
+            IndexOutOfBoundsException,
+            UnsupportedOperationException;
+
+    public void editFlight(int index, Flight flight);
+
+    public FlightData getFlightData();
+
+}
