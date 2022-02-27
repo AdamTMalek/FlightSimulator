c9e8e24cdd419918b99a3e267ea3957d87794611
tinumide <summittinuade@gmail.com>
2022-02-27 08:07:17 +0100

    adding screen top

--- src/main/java/com/github/adamtmalek/flightsimulator/GUI/screen.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/GUI/screen.java
@@ -1,4 +1,16 @@
 package com.github.adamtmalek.flightsimulator.GUI;
 
+import javax.swing.*;
+
 public class screen {
+    private JPanel panelTop;
+    private JPanel panelBottom;
+    private JPanel panelTopLeft;
+    private JPanel panelTopRight;
+    private JList flightList;
+    private JList flightPlans;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
 }
