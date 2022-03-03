32039539d6b7f4feb18948580ad8351069ccfdbd
Adam Malek <malekadas@gmail.com>
2022-03-03 23:02:25 +0100

    Update documentation

--- src/main/java/com/github/adamtmalek/flightsimulator/GUI/models/BoundListModel.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/GUI/models/BoundListModel.java
@@ -5,6 +5,16 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.util.List;
 
+/**
+ * The BoundListModel is an implemention of AbstractListModel
+ * @see javax.swing.AbstractListModel
+ * which data source is bounded to the data source passed in the constructor.
+ *
+ * This means, that no local copy of the list exists, and any change to the original
+ * list be reflected in the data stored in this class as well.
+ *
+ * @param <E> Type of elements stored in the list.
+ */
 public class BoundListModel<E> extends AbstractListModel<E> {
 	private final @NotNull List<E> data;
 
