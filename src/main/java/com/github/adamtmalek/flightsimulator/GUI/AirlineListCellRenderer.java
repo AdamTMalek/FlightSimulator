09b3b325f75c1fb6c203a66fde160d34bbcff954
Adam Malek <malekadas@gmail.com>
2022-03-03 19:30:25 +0100

    Display full name of airlines instead of their codes in comboboxes

--- src/main/java/com/github/adamtmalek/flightsimulator/GUI/AirlineListCellRenderer.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/GUI/AirlineListCellRenderer.java
@@ -6,6 +6,6 @@ import org.jetbrains.annotations.NotNull;
 public class AirlineListCellRenderer extends CustomListCellRenderer<Airline> {
 	@Override
 	protected @NotNull String getText(@NotNull Airline value) {
-		return value.code();
+		return value.name();
 	}
 }
