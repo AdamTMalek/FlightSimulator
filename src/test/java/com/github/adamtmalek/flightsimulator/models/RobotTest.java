926b30c5fc197119d3318fc32ce554d7597eaabc
Ade20150 <91483078+Ade20150@users.noreply.github.com>
2022-03-04 13:22:34 +0000

    Add a robot method

--- src/test/java/com/github/adamtmalek/flightsimulator/models/RobotTest.java
+++ src/test/java/com/github/adamtmalek/flightsimulator/models/RobotTest.java
@@ -63,8 +63,6 @@ JTable components5 = slave.getFlightPlanTable();
 		mouseMoveAndClick(location7.x +220, location5.y);
 		robot.keyPress(KeyEvent.VK_DOWN);
 		robot.keyPress(KeyEvent.VK_ENTER);
-
-
 		JButton components2 = slave.getAddButton();
 		Point location2 = components2.getLocationOnScreen();
 		System.out.println(location2.x +", "+location2.y+"");
@@ -78,9 +76,59 @@ JTable components5 = slave.getFlightPlanTable();
 
 	}
 
+	public void getRobot() {
+		//implements the controller interface
+		var flightTrackerController = new FlightTrackerController();
+		slave = new Screen(flightTrackerController);
+		slave.setVisible(true);
+		//simulates a real user accessing and using the GUI
+		JComboBox components1 = slave.getDepartureBox();
+		Point location1 = components1.getLocationOnScreen();
+		System.out.println(location1.x + ", " + location1.y + "");
+		mouseMoveAndClick(location1.x, location1.y);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		JTextField components3 = slave.getFlightNumberTextField();
+		Point location3 = components3.getLocationOnScreen();
+		System.out.println(location3.x +", "+location3.y+"");
+		mouseMoveAndClick(location3.x, location3.y);
+		for (int i = 0; i < 4; i++) {
+			robot.keyPress(KeyEvent.VK_7);
+		}
+		JTable components5 = slave.getFlightPlanTable();
+		Point location5 = components5.getLocationOnScreen();
+		System.out.println(location5.x + ", " + location5.y + "");
+		mouseMoveAndClick(location5.x +5, location5.y);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		JTable components6 = slave.getFlightPlanTable();
+		Point location6 = components6.getLocationOnScreen();
+		System.out.println(location6.x + ", " + location6.y + "");
+		mouseMoveAndClick(location6.x +105, location6.y);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		JTable components7 = slave.getFlightPlanTable();
+		Point location7 = components7.getLocationOnScreen();
+		System.out.println(location7.x + ", " + location7.y + "");
+		mouseMoveAndClick(location7.x +220, location5.y);
+		robot.keyPress(KeyEvent.VK_DOWN);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		JButton components2 = slave.getAddButton();
+		Point location2 = components2.getLocationOnScreen();
+		System.out.println(location2.x +", "+location2.y+"");
+		mouseMoveAndClick(location2.x, location2.y);
+		JButton components4 =slave.getExitButton();
+		Point location4 = components4.getLocationOnScreen();
+		System.out.println(location4.x + ", "+ location4.y+"");
+		mouseMoveAndClick(location4.x, location4.y);
 
+	}
 
-		//creates an object that stimulates a mouse click
+	//creates an object that stimulates a mouse click
 		public void mouseMoveAndClick ( int xLoc, int yLoc){
 			robot.mouseMove(xLoc, yLoc);
 			robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
