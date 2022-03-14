0bc4850106695f2936d35ce40e257459851ff521
APMMonteiro <ap71@hw.ac.uk>
2022-03-14 14:44:19 +0000

    colour formatting added to logger

--- src/main/java/com/github/adamtmalek/flightsimulator/logger/LogLevel.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/logger/LogLevel.java
@@ -1,18 +1,20 @@
 package com.github.adamtmalek.flightsimulator.logger;
 
 public enum LogLevel {
-	ALL		(Integer.MAX_VALUE),
-	TRACE	(600),
-	DEBUG	(500),
-	INFO	(400),
-	WARN	(300),
-	ERROR	(200),
-	FATAL	(100),
-	OFF		(0);
+	ALL		(Integer.MAX_VALUE, ""),
+	TRACE	(600, ""),
+	DEBUG	(500, ""),
+	INFO	(400, ""),
+	WARN	(300, "\033[0;33m"),
+	ERROR	(200, "\033[0;31m"),
+	FATAL	(100, "\033[1;31m"),
+	OFF		(0, "");
 
 	public final int value;
+	public final String colourCode;
 
-	LogLevel(int v) {
+	LogLevel(int v, String c) {
 		this.value = v;
+		this.colourCode = c;
 	}
 }
--- src/main/java/com/github/adamtmalek/flightsimulator/logger/Logger.java
+++ src/main/java/com/github/adamtmalek/flightsimulator/logger/Logger.java
@@ -2,6 +2,8 @@ package com.github.adamtmalek.flightsimulator.logger;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public class Logger {
 	private static final Logger logger = new Logger();
 
@@ -25,8 +27,16 @@ public class Logger {
 
 	public void log(@NotNull LogLevel level, String msg) {
 		if (level.value > this.loggerLevel.value) {return;}
+		String logmsg = String.format(
+			"%s[%s]: %s%s",
+			level.colourCode,
+			level.name(),
+			msg,
+			"\033[0m"
+			);
 		// DO STUFF
-		System.out.println(msg);
+		System.out.println(logmsg);
+		// Arrays.toString(Thread.currentThread().getStackTrace());
 	}
 	public void trace(String msg) {
 		logger.log(LogLevel.TRACE, msg);
