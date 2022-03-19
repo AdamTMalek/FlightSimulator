8479c96a0704c057b1d544065838796a48694b84
Adam Malek <malekadas@gmail.com>
2022-03-19 20:25:34 +0100

    Add Mockito and AssertJ Swing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     compileOnly("org.jetbrains:annotations:23.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.assertj:assertj-swing-junit:3.9.2")
+    testImplementation("org.mockito:mockito-core:4.4.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
