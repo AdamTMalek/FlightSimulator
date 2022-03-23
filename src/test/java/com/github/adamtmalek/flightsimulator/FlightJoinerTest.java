a388ff886a913a3996a5d7846604b6b0519334d1
Chris Hulme <chris_hulme99@outlook.com>
2022-03-23 10:42:55 +0000

    Created test for flighting between towers

--- src/test/java/com/github/adamtmalek/flightsimulator/FlightJoinerTest.java
+++ src/test/java/com/github/adamtmalek/flightsimulator/FlightJoinerTest.java
@@ -11,13 +11,71 @@ import java.util.ArrayList;
 
 public class FlightJoinerTest {
 
+	@Test
+	void testJoinFlightBetweenControlTowers() {
+
+		// Configure thread timing for test case.
+		FlightSimulationThreadManagement.setFlightSimulationFrequency(0.05); // Flight travelling between G-E within test duration.
+		FlightSimulationThreadManagement.setThreadFrequency(1.9);
+		FlightSimulationThreadManagement.setGuiUpdateFrequency(0.508);
+
+
+		var glasgowAirport = new Airport("G", "Glasgow Airport", new GeodeticCoordinate(55.87, -4.43));
+		var edinburghAirport = new Airport("E", "Edinburgh Airport", new GeodeticCoordinate(55.95, -3.19));
+		var londonAirport = new Airport("L", "London Airport", new GeodeticCoordinate(51.47, -0.46));
+		var newYorkAirport = new Airport("NY", "New York Airport", new GeodeticCoordinate(40.71, -74.01));
+
+		var joiner = new FlightJoiner();
+
+		glasgowAirport.controlTower.registerSubscriber(joiner);
+		edinburghAirport.controlTower.registerSubscriber(joiner);
+		londonAirport.controlTower.registerSubscriber(joiner);
+		newYorkAirport.controlTower.registerSubscriber(joiner);
+
+		var stubJoinerSubscriber = new StubFlightJoinerSubscriber();
+		joiner.registerSubscriber(stubJoinerSubscriber);
+
+		var tracker = new Thread(new FlightTracker(Flight.buildWithSerialNumber("001",
+				new Airline("", ""),
+				new Aeroplane("a", "a", 1, 50),
+				glasgowAirport,
+				newYorkAirport,
+				ZonedDateTime.of(2022, 2, 18, 16, 0, 0, 0, ZoneId.of("UTC+0")),
+				new ArrayList<Airport.ControlTower>() {{
+					add(glasgowAirport.controlTower);
+					add(edinburghAirport.controlTower);
+					add(londonAirport.controlTower);
+					add(newYorkAirport.controlTower);
+				}})));
+
+
+		var joinerThread = new Thread(joiner);
+
+		tracker.start();
+		joinerThread.start();
+		try {
+			Thread.sleep(2000);
+			tracker.stop();
+			joinerThread.stop();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		final var stubJoinerSubscriberData = stubJoinerSubscriber.receivedData;
+		Assertions.assertEquals(1, stubJoinerSubscriberData.size());
+		Assertions.assertEquals("E", stubJoinerSubscriberData.get(0).controlTowerCode());
+		Assertions.assertEquals(55.91264599913692, stubJoinerSubscriberData.get(0).flight().estimatedPosition().latitude());
+		Assertions.assertEquals(-3.7937437059544337, stubJoinerSubscriberData.get(0).flight().estimatedPosition().longitude());
+
+	}
 
 	@Test
 	void testJoinFlightAfterPassingControlTower() {
 
 		// Configure thread timing for test case.
-		FlightSimulationThreadManagement.setFlightSimulationFrequency(0.005);
-		FlightSimulationThreadManagement.setThreadFrequency(2);
+		FlightSimulationThreadManagement.setFlightSimulationFrequency(0.005); // Flight travelling between G-E-L within test duration.
+		FlightSimulationThreadManagement.setThreadFrequency(1.9);
 		FlightSimulationThreadManagement.setGuiUpdateFrequency(0.508);
 
 
